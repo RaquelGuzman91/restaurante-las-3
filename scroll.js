58dccdd0e98d8ac53ef5e52ccdc5c0f5af05e2ab
RaquelGuzman91 <raquelguzman91@gmail.com>
2025-12-12 20:27:42 -0600

    cambios a informacion e imagenes

--- scroll.js
+++ scroll.js
@@ -6,7 +6,7 @@ ScrollReveal({
 });
 
 
-ScrollReveal().reveal('.banner-text, .banner ' , { origin: 'bottom'});
-ScrollReveal().reveal('.slogan , .menu'  , {origin: 'right'});
-ScrollReveal().reveal('.promotions , .our', {origin: 'left'});
-ScrollReveal().reveal('.contact , .section-link ', {origin: 'top'});
+ScrollReveal().reveal('.banner-text h3, .banner p , .icon-a' , { origin: 'bottom'});
+ScrollReveal().reveal('.slogan , .menu , .our-text h2 p'  , {origin: 'right'});
+ScrollReveal().reveal('.promotions h2 , .our-text', {origin: 'left'});
+ScrollReveal().reveal('.contact h3, .contact h4, .contact p , .icon-i , .section-link ', {origin: 'top'});
